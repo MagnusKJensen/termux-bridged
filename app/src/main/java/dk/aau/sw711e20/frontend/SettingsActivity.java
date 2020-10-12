e9cf9d1bced38ad69ab8eeb2ca5fc448ad33b9c0
Hannah Lockey <hlocke17@student.aau.dk>
2020-10-12 11:26:55 +0200

    Can now check if phone is charging and/or connected to wifi

--- app/src/main/java/dk/aau/sw711e20/frontend/SettingsActivity.java
+++ app/src/main/java/dk/aau/sw711e20/frontend/SettingsActivity.java
@@ -2,9 +2,18 @@ package dk.aau.sw711e20.frontend;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.termux.R;
 
@@ -17,6 +26,7 @@ public class SettingsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+
     }
 
     @SuppressLint("SetTextI18n")
@@ -28,6 +38,11 @@ public class SettingsActivity extends Activity {
             wifi_button.setText("Always");
         } else if (wifi_button.getText().equals("Always")) {
             wifi_button.setText("On");
+            if (isConnectedToWifi()) {
+                Toast.makeText(getApplicationContext(), "is connected to wifi", Toast.LENGTH_SHORT).show();
+            } else if (!isConnectedToWifi()) {
+                Toast.makeText(getApplicationContext(), "not connected to wifi", Toast.LENGTH_SHORT).show();
+            }
         } else {
             wifi_button.setText("you should not see this text");
         }
@@ -42,8 +57,28 @@ public class SettingsActivity extends Activity {
             power_button.setText("Always");
         } else if (power_button.getText().equals("Always")) {
             power_button.setText("To power");
+            if (isCharging()) {
+                Toast.makeText(getApplicationContext(), "phone is charging", Toast.LENGTH_SHORT).show();
+            } else if (!isCharging()) {
+                Toast.makeText(getApplicationContext(), "phone is not charging", Toast.LENGTH_SHORT).show();
+            }
         } else {
             power_button.setText("you should not see this text");
         }
+    }
+
+    public boolean isCharging() {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+
+        assert batteryStatus != null;
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
+    }
+
+    public boolean isConnectedToWifi() {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connManager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnected();
     }
 }
